bcdb8d37322f1c11b4b6a665cd4dcd5084489876
jjo <k01066624566@gmail.com>
2021-08-17 20:44:53 +0900

    add 선택정렬

---
+++ Algorithm/sort/선택정렬/선택정렬.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void selection_sort(int *arr,int arr_len){
+  for(int i=0;i<arr_len;i++){
+    int min=1000,min_index;
+    for(int j=i;j<arr_len;j++){
+      if(min>arr[j]){
+        min=arr[j];
+        min_index=j;
+      }
+    }
+    int temp=arr[i];
+    arr[i]=min;
+    arr[min_index]=temp;
+  }
+}
+int main(){
+  int arr[5]={2,9,5,3,8};
+  selection_sort(arr,5);
+  for(int i=0;i<5;i++){
+    printf("%d ",arr[i]);
+  }
+}
