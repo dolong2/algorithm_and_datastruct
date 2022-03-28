f374c88873a9970580da4a5adc865521f4543ab7
jeayoung <k01066624566@gmail.com>
2022-03-28 18:05:12 +0900

    fix

--- Algorithm/sort/버블정렬/buble.cpp
+++ Algorithm/sort/버블정렬/buble.cpp
@@ -1,19 +1,18 @@
-#include<stdio.h>
-void buble_sort(int *arr,int arr_len){
-  for(int i=0;i<arr_len;i++){
-    for(int j=0;j<arr_len-1;j++){
-      if(arr[j]>arr[j+1]){
-        int temp=arr[j+1];
-        arr[j+1]=arr[j];
+#include<iostream>
+using namespace std;
+void bubble_sort(int *arr,int arr_len){
+  for(int i=arr_len-1;i>=0;i--){
+    for(int j=0;j<i;j++){
+      if(arr[i]<arr[j]){
+        int temp=arr[i];
+        arr[i]=arr[j];
         arr[j]=temp;
       }
     }
   }
 }
 int main(){
-  int arr[5]={2,9,5,3,8};
-  buble_sort(arr,5);
-  for(int i=0;i<5;i++){
-    printf("%d ",arr[i]);
-  }
+  int arr[6]={2,9,5,3,8,1};
+  bubble_sort(arr,6);
+  for(int i=0;i<6;i++){printf("%d ",arr[i]);}
 }
